0faa94a3b23778165c9bda906608d43b0090e81a
Shokhruzbek Kakharov <shokhruzbekkakharov@college.harvard.edu>
2025-03-12 04:40:48 -0400

    wl34567901 commit

--- network_shadow.c
+++ network_shadow.c
@@ -220,16 +220,11 @@ static int __init network_shadow_init(void)
     shadow->netdev_notifier.notifier_call = netdev_event;
     register_netdevice_notifier(&shadow->netdev_notifier);
     
-    /* Create proc entry - use create_proc_entry for older kernels */
-#if LINUX_VERSION_CODE < KERNEL_VERSION(3,10,0)
+    /* Create proc entry - use fully compatible method for older kernels */
     proc_entry = create_proc_entry("network_shadow", 0644, NULL);
-    if (proc_entry)
+    if (proc_entry) {
         proc_entry->proc_fops = &shadow_proc_fops;
-#else
-    proc_entry = proc_create("network_shadow", 0644, NULL, &shadow_proc_fops);
-#endif
-    
-    if (!proc_entry) {
+    } else {
         unregister_netdevice_notifier(&shadow->netdev_notifier);
         kfree(shadow);
         return -ENOMEM;
